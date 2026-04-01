87e028110b2131f87ce6306080a2ca811fe5bcd0
zwang2019 <zhao.wang.unsw@gmail.com>
2026-04-01 16:00:12 +0800

    python spider learning

---
+++ python_spider/cURL_to_requests.py
@@ -0,0 +1,33 @@
+# https://spidertools.cn/ convert cURL to python requests
+
+import requests
+
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+    "sec-ch-ua": "\"Chromium\";v=\"146\", \"Not-A.Brand\";v=\"24\", \"Google Chrome\";v=\"146\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\""
+}
+cookies = {
+    "https_waf_cookie": "eb6c0e22-0e66-4972ae307d0ef776f3352145e4055622e5fa",
+    "Hm_lvt_f9e56acddd5155c92b9b5499ff966848": "1774978634,1775029941",
+    "Hm_lpvt_f9e56acddd5155c92b9b5499ff966848": "1775029941",
+    "HMACCOUNT": "DF75B59B5EB75318"
+}
+url = "https://www.89ip.cn/"
+response = requests.get(url, headers=headers, cookies=cookies)
+
+print(response.text)
+print(response)
+
--- python_spider/spyder_learn.py
+++ python_spider/spyder_learn.py
@@ -2,111 +2,111 @@ import requests
 from retrying import retry
 
 
-# # requests.get
-# response = requests.get('https://api.github.com')
-# print('response:', response)
-# print('response.status_code:', response.status_code)
-# print('response.text:', response.text)
-# print('response.content:', response.content)    # download files like videos, pictures and musics...
-# print('response.content decoded:', response.content.decode('utf-8'))    # using response.content.decode('utf-8') to decode the data    # 处理中文乱码!!!
-# print('response.headers:', response.headers)
-# print('response.cookies:', response.cookies)
-# print('response.encoding:', response.encoding)
-# response.encoding = 'utf-8'
-# print('response.text:', response.text)    # using response.encoding to decode the data
-#
-# try:
-#     print('response.json:', response.json())    # only for JSON data, it will decode the data to dict
-#     print(type(response.json()))
-# except:
-#     pass
-# print('response.request.headers:', response.request.headers)
-#
-#
-# # requests.post
-# data = {
-#     "username": "admin",
-#     "password": "123456",
-# }
-# response = requests.post('https://api.github.com', data=data)
-# print(response)
-#
-#
+# requests.get
+response = requests.get('https://api.github.com')
+print('response:', response)
+print('response.status_code:', response.status_code)
+print('response.text:', response.text)
+print('response.content:', response.content)    # download files like videos, pictures and musics...
+print('response.content decoded:', response.content.decode('utf-8'))    # using response.content.decode('utf-8') to decode the data    # 处理中文乱码!!!
+print('response.headers:', response.headers)
+print('response.cookies:', response.cookies)
+print('response.encoding:', response.encoding)
+response.encoding = 'utf-8'
+print('response.text:', response.text)    # using response.encoding to decode the data
+
+try:
+    print('response.json:', response.json())    # only for JSON data, it will decode the data to dict
+    print(type(response.json()))
+except:
+    pass
+print('response.request.headers:', response.request.headers)
+
+
+# requests.post
+data = {
+    "username": "admin",
+    "password": "123456",
+}
+response = requests.post('https://api.github.com', data=data)
+print(response)
+
+
 # To simulate browser operations, need to add request headers. !!!
-# headers = {
-#     "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-#     "accept-encoding" : "gzip, deflate, br, zstd",
-#     "accept-language" : "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-#     "cache-control" : "no-cache",
-#
-#
-#
-#     "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
-# }
-#
-# response = requests.get("https://www.baidu.com")
-# print('response without header: ', response)
-# print('response without header: ', response.text)
-# print('#' * 500)
-# response = requests.get("https://www.baidu.com", headers=headers)
-# print('response with header: ', response)
-# print('response with header: ', response.text)
-# print('#' * 500)
-#
-# # redirect
-# response_ar = requests.get("https://www.baidu.com", headers=headers, allow_redirects=True)
-# cookies_ar = response_ar.cookies
-#
-# print('cookie: ', cookies_ar)
-# print('text: ', response_ar.text)
-# print('code: ', response_ar.status_code)
-#
-# print('#' * 500)
-# response_no_ar = requests.get("https://www.bilibili.com", headers=headers, allow_redirects=False)
-# cookies_no_ar = response_no_ar.cookies
-#
-#
-# # request with cookie
-#
-#
-# cookies = {
-#     '__cfduid': 'dfaedafedasfeas',
-# }
-#
-# response = requests.get("https://www.baidu.com", cookies=cookies)
-# print('response with cookies: ', response)
-# print('response with cookies: ', response.text)
-#
-#
-# # session: session can save cookies, headers and other data, which can be used for multiple requests. !!!
-#
-# headers = {
-#     "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-#     "accept-encoding" : "gzip, deflate, br, zstd",
-#     "accept-language" : "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-#     "cache-control" : "no-cache",
-#     "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
-# }
-#
-# session = requests.session()
-# res = session.get('https://www.baidu.com',headers=headers)
-# print('session cookies: ', res.cookies)
-# res_2 = session.post('https://www.baidu.com',headers=headers)
-# print('request headers: ', res_2.request.headers)
-#
-# print('convert cookies from cookiejar to dict: ', requests.utils.dict_from_cookiejar(res.cookies))
-#
-# res_3 = session.get('https://www.baidu.com',headers=headers,verify=False)   # verify=False can ignore SSL certificate verification, but it is not recommended to use it in production environment, because it will make your application vulnerable to man-in-the-middle attacks. !!!
-#
-#
-# # session proxy: hide
-# proxies = {
-#     "http" : "http://8.138.131.110:3128",
-#     "https" : "https://8.138.131.110:3128",    # when using https, it returns real ip, because the website only support http, it only matches http proxy, so it will return real ip when using https. !!!
-# }
-# response = requests.get('http://httpbin.org/ip',proxies=proxies, timeout=5)
-# print(response.text)
-#
+headers = {
+    "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding" : "gzip, deflate, br, zstd",
+    "accept-language" : "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "cache-control" : "no-cache",
+
+
+
+    "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+}
+
+response = requests.get("https://www.baidu.com")
+print('response without header: ', response)
+print('response without header: ', response.text)
+print('#' * 500)
+response = requests.get("https://www.baidu.com", headers=headers)
+print('response with header: ', response)
+print('response with header: ', response.text)
+print('#' * 500)
+
+# redirect
+response_ar = requests.get("https://www.baidu.com", headers=headers, allow_redirects=True)
+cookies_ar = response_ar.cookies
+
+print('cookie: ', cookies_ar)
+print('text: ', response_ar.text)
+print('code: ', response_ar.status_code)
+
+print('#' * 500)
+response_no_ar = requests.get("https://www.bilibili.com", headers=headers, allow_redirects=False)
+cookies_no_ar = response_no_ar.cookies
+
+
+# request with cookie
+
+
+cookies = {
+    '__cfduid': 'dfaedafedasfeas',
+}
+
+response = requests.get("https://www.baidu.com", cookies=cookies)
+print('response with cookies: ', response)
+print('response with cookies: ', response.text)
+
+
+# session: session can save cookies, headers and other data, which can be used for multiple requests. !!!
+
+headers = {
+    "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding" : "gzip, deflate, br, zstd",
+    "accept-language" : "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "cache-control" : "no-cache",
+    "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+}
+
+session = requests.session()
+res = session.get('https://www.baidu.com',headers=headers)
+print('session cookies: ', res.cookies)
+res_2 = session.post('https://www.baidu.com',headers=headers)
+print('request headers: ', res_2.request.headers)
+
+print('convert cookies from cookiejar to dict: ', requests.utils.dict_from_cookiejar(res.cookies))
+
+res_3 = session.get('https://www.baidu.com',headers=headers,verify=False)   # verify=False can ignore SSL certificate verification, but it is not recommended to use it in production environment, because it will make your application vulnerable to man-in-the-middle attacks. !!!
+
+
+# session proxy: hide
+proxies = {
+    "http" : "http://8.138.131.110:3128",
+    "https" : "https://8.138.131.110:3128",    # when using https, it returns real ip, because the website only support http, it only matches http proxy, so it will return real ip when using https. !!!
+}
+response = requests.get('http://httpbin.org/ip',proxies=proxies, timeout=5)
+print(response.text)
+
 
 # retry can be implemented in at least four ways.
 # 1. if/else
