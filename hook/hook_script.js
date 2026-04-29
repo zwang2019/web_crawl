cff26d15036ccc3ac48664a6cc5bf294b7670c1c
zwang2019 <zhao.wang.unsw@gmail.com>
2026-04-29 19:35:00 +0800

    hook learn

--- hook/hook_script.js
+++ hook/hook_script.js
@@ -16,9 +16,94 @@ JSON = {
     }
 }
 
+// stringify: for request encryption: for RSA/AES encryption. obj stringify to str then encrypt
+// parse: when response has been decrypted, for response decryption: for RSA/AES decryption. decrypt to str then parse to obj
 
 j_s = '{"name": "alice"}'
 
 console.log("Result of JSON.parse: ", JSON.parse(j_s));
+
+
+
+/* -------------------------------------------------------------------------------------*/
+// XHR request hook
+(function() {
+    var my_open = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function(method,url,async) {
+        if (url.indexOf("data") !== -1){
+            console.log("Hooked XMLHttpRequest.open called with arguments:", method, url);
+            debugger;
+        }
+        return my_open.apply(this, arguments);
+    }
+})();
+
+/* -------------------------------------------------------------------------------------*/
+// hook header when header has encryption.
+(function(){
+    var sh = window.XMLHttpRequest.prototype.setRequestHeader;
+    window.XMLHttpRequest.prototype.setRequestHeader = function (key, value) {
+        if (key === 'chromosome' || key === 'Token') {
+            console.log("Hooked XMLHttpRequest.setRequestHeader called with arguments:", key, value);
+            debugger;
+        }
+        return sh.apply(this, arguments);
+    }
+})();
+
+/* -------------------------------------------------------------------------------------*/
+// hook cookie when cookie has encryption and changes in each request. (most used)
+(function (){
+    var cookieTemp = '';
+    Object.defineProperty(document, 'cookie', {
+        set: function (val) {
+            if (val.indexOf('ads-tracker-baidu') !== -1) {
+                console.log('Hook cookie set->', val);
+                debugger;
+            }
+            cookieTemp = val;
+            return val;
+        },
+        get: function () {
+            return cookieTemp;
+        }
+    });
+})();
+
+/* -------------------------------------------------------------------------------------*/
+// methods empty
+function deg(){
+    debugger;
+}
+deg = function(){};
+
+setInterval = function(){};
+setTimeout = function(){};
+
+/* -------------------------------------------------------------------------------------*/
+// hook window, window.close anti debug
+(function (){
+    window.close = function (arg) {
+        console.log("Hooked window.close called with argument:", arg);
+        debugger;
+        return null;
+    }
+})();
+
+/* -------------------------------------------------------------------------------------*/
+// hook history, history.back anti debug
+(function(){
+    history.back = function () {
+        console.log("Hooked history.back called");
+        debugger;
+        return null;
+    }
+})();
+
+/* -------------------------------------------------------------------------------------*/
+
+
+
+
 
 
