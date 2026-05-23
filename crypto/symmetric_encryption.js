4b2ab8c5dd6012656a860302b03403412b8ba067
zwang2019 <zhao.wang.unsw@gmail.com>
2026-05-24 01:15:35 +0800

    Symmetric Encryption

--- crypto/symmetric_encryption.js
+++ crypto/symmetric_encryption.js
@@ -1,3 +1,33 @@
 const Crypto = require('crypto-js');
 /* -------------------------------------------------------------------------------------*/
 //DES
+
+var key = Crypto.enc.Utf8.parse('theosophist');
+console.log(`word_array key is: `, key);
+var iv = Crypto.enc.Utf8.parse('thievish');
+console.log(`word_array iv is: `, iv);
+var text = Crypto.enc.Utf8.parse('thistle');
+
+des_encrypted = Crypto.DES.encrypt(text, key,
+    {
+            iv: iv,
+            mode: Crypto.mode.CBC,
+            padding: Crypto.pad.Pkcs7
+        }
+    ).toString();
+
+console.log(`DES encrypted text is: ${des_encrypted}`);
+
+
+/* -------------------------------------------------------------------------------------*/
+//AES
+
+aes_encrypted = Crypto.AES.encrypt(text, key,
+    {
+            iv: iv,
+            mode: Crypto.mode.CBC,
+            padding: Crypto.pad.Pkcs7
+        }
+    ).toString();
+
+console.log(`AES encrypted text is: ${aes_encrypted}`);
