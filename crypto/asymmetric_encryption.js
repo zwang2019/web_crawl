58634ef569a05b666a0c47e27454161a72990b83
zwang2019 <zhao.wang.unsw@gmail.com>
2026-05-25 00:37:37 +0800

    Asymmetric Encryption

--- crypto/asymmetric_encryption.js
+++ crypto/asymmetric_encryption.js
@@ -27,3 +27,29 @@ console.log(`Encrypted message is: ${enc_mess}`);
 var dec_mess = decryptMessage(enc_mess, privateKey);
 console.log(`Decrypted message is: ${dec_mess}`);
 
+/* -------------------------------------------------------------------------------------*/
+// RSA node-forge library
+
+const forge = require('node-forge');
+
+// encrypt with public key
+function forgeencrypt(message, publicKey){
+    publicKey = forge.pki.publicKeyFromPem(publicKey);
+    var encrypted = publicKey.encrypt(message, 'RSA-OAEP');
+    return forge.util.encode64(encrypted);    // using forge library have to manually encode with base64
+}
+
+// decrypt with private key
+function forgeDecrypt(encryptedMessage, privateKey){
+    privateKey = forge.pki.privateKeyFromPem(privateKey);
+    var encryptedBytes = forge.util.decode64(encryptedMessage);  // using forge library have to manually decode with base64
+    return privateKey.decrypt(encryptedBytes, 'RSA-OAEP');
+}
+
+var forge_enc_mess = forgeencrypt(data, publicKey);
+console.log(`Encrypted message is: ${forge_enc_mess}`);
+
+var forge_dec_mess = forgeDecrypt(forge_enc_mess, privateKey);
+console.log(`Decrypted message is: ${forge_dec_mess}`);
+
+/* -------------------------------------------------------------------------------------*/
